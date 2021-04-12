98b64772cdb4d930a430e783eae2183bebdcded7
Cesar Augusto da Silva <c.augusto@btp.com.br>
2021-04-12 14:24:10 -0300

    Add files

---
+++ Teste1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Teste1
+{
+    class Program
+    {
+        //TODO
+        /* 
+         * Desenvolva um método que receba a string "abbcbbb". O retorno desse método deverá ser "abcb", 
+         * logo o próximo caracter não deverá ser igual ao anterior, o método deve ser capaz de tratar qualquer string.
+        */
+
+        static void Main(string[] args)
+        {
+            String avaliacao = "abbcbbb";
+        }
+    }
+}
